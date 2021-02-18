51f792c9ce42b04627d825eab6ad5170b2f9ad08
TinyRogue <oskarmikus@protonmail.com>
2021-02-18 13:12:48 +0100

    Moved field_t to shared_data to kepp DRY principle

--- host/labyrinth-loader/labyrinth-loader.h
+++ host/labyrinth-loader/labyrinth-loader.h
@@ -1,19 +1,12 @@
 #ifndef GOGGLE_EYED_APPROACH_APPLICATION_H
 #define GOGGLE_EYED_APPROACH_APPLICATION_H
 
-#include <stdio.h>      /*  For file handling           */
-#include <stdbool.h>    /*  For error handling          */
-#include <string.h>     /*  For error handling          */
-#include <errno.h>      /*  For error handling          */
-#include <stdlib.h>     /*  For exit(), malloc() family */
-
-
-typedef enum {BLANK, WALL, BUSH, COIN, TREASURE, LARGE_TREASURE, DROPPED_TREASURE, CAMPSITE, SPAWN} field_kind_t;
-
-typedef struct {
-    size_t value;
-    field_kind_t kind;
-} field_t;
+#include <stdio.h>                      /*  For file handling           */
+#include <stdbool.h>                    /*  For error handling          */
+#include <string.h>                     /*  For error handling          */
+#include <errno.h>                      /*  For error handling          */
+#include <stdlib.h>                     /*  For exit(), malloc() family */
+#include "../../utils/shared_data.h"    /* For field_kind_t and field_t */
 
 typedef struct {
     field_t **lbrth;
---
+++ utils/shared_data.h
@@ -0,0 +1,49 @@
+#ifndef SHARED_DATA_H
+#define SHARED_DATA_H
+
+#include <semaphore.h>
+
+
+#define PLAYER_LISTNER_SEM "/PLAYER_LSTNR_SEM"
+#define HOST_LISTENER_SEM "/HOST_LSTNR_SEM"
+#define HOST_PLAYER_PIPE "/HP_PIPE"
+#define PLAYER_SIGHT 3
+
+typedef enum {BLANK, WALL, BUSH, COIN, TREASURE, LARGE_TREASURE, DROPPED_TREASURE, CAMPSITE, SPAWN} field_kind_t;
+typedef enum {CPU, HUMAN} player_kind_t;
+typedef enum {EAST, NORTH, WEST, SOUTH} dir_t;
+
+typedef struct {
+    size_t value;
+    field_kind_t kind;
+} field_t;
+
+typedef struct {
+    int pid;
+    int host_pid;
+    int carried_treasure;
+    int brought_treasure;
+    player_kind_t player_kind;
+    field_t player_sh_lbrth[PLAYER_SIGHT][PLAYER_SIGHT];
+    size_t current_round;
+    struct {
+        int pos_x;
+        int pos_y;
+    };
+    dir_t dir;
+    sem_t host_response;
+    sem_t player_response;
+} shared_info_t;
+
+typedef struct {
+    sem_t *player_response_sem;
+    sem_t *host_response_sem;
+    struct comm_shm {
+        size_t pid;
+        char shm_name[40];
+        bool can_join;
+        char additional_info[100];
+    } *comm_shm;
+} comms_t;
+
+#endif
