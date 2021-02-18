9a81a61dc25e3bc24a0e886d41de7a3cbe807fcb
TinyRogue <oskarmikus@protonmail.com>
2021-02-18 21:11:33 +0100

    Added logger feature

--- host/view/view.c
+++ host/view/view.c
@@ -1,5 +1,6 @@
 #include "view.h"
 
+
 static void print_background() {
     int width = 0, height = 0;
     getmaxyx(stdscr, height, width);
@@ -10,6 +11,7 @@ static void print_background() {
         }
     }
 }
+
 
 void init_screen() {
     initscr();
@@ -35,8 +37,8 @@ void init_screen() {
     init_pair(BEAST_COLOUR, COLOR_BLACK, COLOR_RED);
     init_pair(TREASURE_COLOUR, COLOR_BLACK, COLOR_YELLOW);
     init_pair(CAMPSITE_COLOUR, COLOR_BLACK, COLOR_GREEN);
-    init_pair(PLAYER_COLOUR, COLOR_BLUE, COLOR_MAGENTA);
-    init_pair(TEXT_COLOUR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(PLAYER_COLOUR, COLOR_WHITE, COLOR_BLUE);
+    init_pair(TEXT_COLOUR, COLOR_BLACK, COLOR_WHITE);
     init_pair(BEAST_COLOUR, COLOR_YELLOW, COLOR_RED);
 
     print_background();
@@ -112,4 +114,119 @@ void print(const char* const message, const int row, const int column) {
     mvprintw(row, column, "%s", message);
     attroff(COLOR_PAIR(TEXT_COLOUR));
     refresh();
+}
+
+void print_coloured(const char* const message, const colours_t colour, const int row, const int column) {
+    attron(COLOR_PAIR(colour));
+    mvprintw(row, column, "%s", message);
+    attroff(COLOR_PAIR(colour));
+    refresh();
+}
+
+void print_legend(int row, int column) {
+    const char *legend[] = {
+        "Legend: ",
+        "1234", " - Players",
+        " " , " - Wall:",
+        "#", " - Bushes",
+        "*", " - Enemy:",
+        "c", " - One coin",
+        "t", " - Treasure (5 coins)",
+        "T", " - Large treasure (10 coins)",
+        "A", " - Campsite", NULL
+    };
+
+    const colours_t next_colours[] = {
+        TEXT_COLOUR,
+        PLAYER_COLOUR, TEXT_COLOUR,
+        WALL_COLOUR, TEXT_COLOUR,
+        BUSHES_COLOUR, TEXT_COLOUR,
+        BEAST_COLOUR, TEXT_COLOUR,
+        TREASURE_COLOUR, TEXT_COLOUR,
+        TREASURE_COLOUR, TEXT_COLOUR, 
+        TREASURE_COLOUR, TEXT_COLOUR,
+        CAMPSITE_COLOUR, TEXT_COLOUR
+    };
+
+    const int text_position[] = {
+        column, row,
+        column, row + 1, column + strlen("1234"), row + 1,
+        column, row + 2, column + strlen(" "), row + 2,
+        column, row + 3, column + strlen("#"), row + 3,
+        column, row + 4, column + strlen("*"), row + 4,
+        column, row + 5, column + strlen("c"), row + 5,
+        column, row + 6, column + strlen("t"), row + 6,
+        column, row + 7, column + strlen("T"), row + 7,
+        column, row + 8, column + strlen("A"), row + 8,
+    };
+    
+    for (int i = 0; *(legend + i); i++) {
+        print_coloured(*(legend + i), next_colours[i], text_position[i * 2 + 1], text_position[i * 2]);
+    }
+    refresh();
+}
+
+// Logger section
+static logger_t logger;
+pthread_mutex_t logger_lock = PTHREAD_MUTEX_INITIALIZER;
+
+bool init_logger(int row, int column, int message_amount, int message_size) {
+    logger.pos_x = column;
+    logger.pos_y = row;
+    logger.message_amount = message_amount;
+    logger.message_size = message_size;
+
+    logger.messages = (char**)calloc(logger.message_amount, sizeof(char*));
+    if (!logger.messages) return false;
+
+    for (int i = 0; i < logger.message_amount; i++) {
+        *(logger.messages + i) = (char*)calloc(logger.message_size, sizeof(char));
+        
+        if (!*(logger.messages + i)) {
+            for (int j = 0; j < i; j++) {
+                free(*(logger.messages + i));
+            }
+            free(logger.messages);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+void log_message(const char * const message) {
+    time_t rawtime;
+    char current_time[10] = {"\0"};
+
+    pthread_mutex_lock(&logger_lock);
+    time (&rawtime);
+    strftime(current_time, 10,"%X> ", localtime(&rawtime));
+
+    for (int i = logger.message_amount - 1; i > 0; i--) {
+        strcpy(logger.messages[i], logger.messages[i - 1]);
+    }
+
+    strncat(strcpy(logger.messages[0], current_time), message, logger.message_size - 11);
+    
+    /*LINE BELOW:
+        Deceiving ncurses line is longer to avoid clearing screen.
+        Clearing screen causes mighty screen flashing.
+    */
+    strncat(logger.messages[0], "                              ", logger.message_size - 1 - strlen(logger.messages[0]));
+
+    print_coloured("MESSAGES", TEXT_COLOUR, logger.pos_y, logger.pos_x);
+    for (int i = 0; i < logger.message_amount; i++) {
+        print_coloured(logger.messages[i], TEXT_COLOUR, logger.pos_y + i + 1, logger.pos_x);
+    }
+    pthread_mutex_unlock(&logger_lock);
+}
+
+
+void destroy_logger() {
+
+    for (int i = 0; i < logger.message_amount; i++) {
+        free(*(logger.messages + i));
+    }
+    free(logger.messages);
 }
